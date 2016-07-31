9cc28ecdd3e1b87d84050f401c106bd1034e10b3
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2016-07-31 16:35:46 +0200

    Orange @domain step /Given a counter with ID :id and a value of :value was added to the collection/ Issue #4 #5 #6

--- tests/behat/features/bootstrap/FeatureContext.php
+++ tests/behat/features/bootstrap/FeatureContext.php
@@ -7,10 +7,13 @@ use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
 use Behat\WebApiExtension\Context\WebApiContext;
 
+use OpenCounter\Domain\Model\Counter\CounterId;
+use OpenCounter\Domain\Model\Counter\CounterValue;
+use OpenCounter\Domain\Model\Counter\Counter;
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends WebApiContext implements Context, SnippetAcceptingContext
+class FeatureContext implements Context, SnippetAcceptingContext
 {
     /**
      * Initializes context.
@@ -19,31 +22,49 @@ class FeatureContext extends WebApiContext implements Context, SnippetAcceptingC
      * You can also pass arbitrary arguments to the
      * context constructor through behat.yml.
      */
-    public function __construct()
+    public function __construct( )
     {
+
+      //$this->catalogue = new CounterRepository($pdo);
     }
 
     /**
-     * @Given a counter with ID :arg1 and a value of :arg2 was added to the collection
+     * @Given a counter with ID :id and a value of :value was added to the collection
      */
-    public function aCounterWithIdAndAValueOfWasAddedToTheCollection($arg1, $arg2)
+    public function aCounterWithIdAndAValueOfWasAddedToTheCollection($id, $value)
     {
-        throw new PendingException();
+      $this->counterID = new CounterId($id);
+      $this->counterValue = new CounterValue($value);
+      $this->counter = new Counter($this->counterID, $this->counterValue, 'passwordplaceholder');
+     // $this->counter->id = $id;
+//      $this->counter->value = $value;
+      //$this->catalogue->add($this->counter);
     }
 
-    /**
-     * @When I get the value of the counter with ID :arg1
-     */
-    public function iGetTheValueOfTheCounterWithId($arg1)
-    {
-        throw new PendingException();
-    }
+//    /**
+//     * @When I get the value of the counter with ID :id
+//     */
+//    public function iGetTheValueOfTheCounterWithId($id)
+//    {
+//
+//
+//// Fetch the counter from the database
+////      $this->counter = $this->catalogue->findByCounterId($id);
+//$this->counterValue = $this->counter->getValue();
+//// Modify the Counter
+//     // $counter->changeProperty('value', '3');
+//
+//// Persist the changes back to the database
+//    //  $this->catalogue->update($counter);
+//    }
 
     /**
      * @Then the value returned should be :arg1
      */
     public function theValueReturnedShouldBe($arg1)
     {
-        throw new PendingException();
+      if (!$arg1 = $this->counterValue) {
+        throw new PHPUnit_Framework_Error_Notice;
+      }
     }
 }
