2937baf2690054109e0be3d7fd775a6b2e292485
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2016-09-09 15:27:10 +0200

    Issue #15 add middleware for real cors

--- configuration/middleware.php
+++ configuration/middleware.php
@@ -2,3 +2,14 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+// middleware for cors for swaggerui
+// https://github.com/tuupola/cors-middleware
+$app->add(new \Tuupola\Middleware\Cors([
+    "origin" => ["*"],
+    "methods" => ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    "headers.allow" => ["Content-Type"],
+    "headers.expose" => [],
+    "credentials" => false,
+    "cache" => 0,
+]));
