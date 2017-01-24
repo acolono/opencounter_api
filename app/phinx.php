6a47d63b9f8a2b350b5c69bbd9ea811a436de491
rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2017-01-24 15:52:52 +0100

    Switch to phinx.php with dotenv for production database setup. Fixes Issue #29

---
+++ app/phinx.php
@@ -0,0 +1,50 @@
+<?php
+
+// load our environment files - used to store credentials & configuration
+(new Dotenv\Dotenv(__DIR__))->load();
+
+return
+    [
+        'paths' => [
+            'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+            'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds',
+        ],
+        'environments' =>
+            [
+                'default_database' => 'development',
+                'default_migration_table' => 'phinxlog',
+                'development'      =>
+                    [
+                        'adapter' => 'mysql',
+                        'host' => (string) getenv('DB_HOST'),
+                        'name' => (string) getenv('MYSQL_DATABASE'),
+                        'user' => (string) getenv('MYSQL_USER'),
+                        'pass' => (string) getenv('MYSQL_PASSWORD'),
+                        'port' => 3306,
+                        'charset' => 'utf8',
+                        'collation' => 'utf8_unicode_ci',
+                    ],
+                'testing' =>
+                    [
+                        'adapter' => 'mysql',
+                        'host' => (string) getenv('DB_HOST'),
+                        'name' => (string) getenv('MYSQL_DATABASE'),
+                        'user' => (string) getenv('MYSQL_USER'),
+                        'pass' => (string) getenv('MYSQL_PASSWORD'),
+                        'port' => 3306,
+                        'charset' => 'utf8',
+                        'collation' => 'utf8_unicode_ci',
+                    ],
+                'production' =>
+                    [
+                        'adapter' => 'mysql',
+                        'host' => (string) getenv('DB_HOST'),
+                        'name' => (string) getenv('MYSQL_DATABASE'),
+                        'user' => (string) getenv('MYSQL_USER'),
+                        'pass' => (string) getenv('MYSQL_PASSWORD'),
+                        'port' => 3306,
+                        'charset' => 'utf8',
+                        'collation' => 'utf8_unicode_ci',
+                    ],
+            ],
+    ];
